614431ec0160173f6e886b59062dd3d5b0975888
Prymushko Oleh <oleg.prim70p@gmail.com>
2023-06-03 11:32:25 +0300

    api pixbay

---
+++ src/api/getSearsh.js
@@ -0,0 +1,16 @@
+const API_KEY = `36992925-08055682156c93c394260c095`;
+const BASE_URL = `https://pixabay.com/api/`;
+const PICS_ON_PAGE = 12;
+
+export const getSearch = (searchText, page) => {
+  const params = new URLSearchParams({
+    q: searchText,
+    page: page,
+    key: API_KEY,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    per_page: PICS_ON_PAGE,
+  });
+
+  return fetch(`${BASE_URL}?${params}`);
+};
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,61 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { getSearch } from 'api/getSearsh';
+
+export class App extends Component {
+  state = {
+    search: '',
+    images: [],
+    page: 1,
+    total: 1,
+    loading: false,
+    error: null,
+    showModal: false,
+    empty: false,
+  };
+  componentDidUpdate(_, PrevState) {
+    if (
+      PrevState.search !== this.state.search ||
+      PrevState.page !== this.state.page
+    ) {
+      this.getFunc(this.state.search, this.state.page);
+    }
+  }
+  getFunc = (text, page) => {
+    this.setState({ loading: true });
+    getSearch(text, page)
+      .then(resp => resp.json())
+      .then(data => {
+        if (data.hits.length === 0) {
+          this.setState({ empty: true });
+        }
+        this.setState(prevSt => ({
+          page: prevSt.page,
+          images: [...prevSt.images, ...data.hits],
+          total: data.total,
+        }));
+      })
+      .catch(error => {
+        this.setState({ error: error.message });
+      })
+      .finally(() => {
+        this.setState({ loading: false });
+      });
+  };
+}
+
+// export const App = () => {
+//   return (
+//     <div
+//       style={{
+//         height: '100vh',
+//         display: 'flex',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         fontSize: 40,
+//         color: '#010101',
+//       }}
+//     >
+//       React homework template
+//     </div>
+//   );
+// };
