2d3c1e35539d212372f0b604ac6f3cafb224591d
Prymushko Oleh <oleg.prim70p@gmail.com>
2023-06-05 12:37:32 +0300

    виправлення дублювання коду

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -49,83 +49,66 @@ export class App extends Component {
     postApiService.page = nextPage;
 
     postApiService.fetchPost().then(data => {
-      postApiService.hits = data.totalHits;
-
-      const newData = data.hits.map(
-        ({ id, tags, webformatURL, largeImageURL }) => ({
-          id,
-          tags,
-          webformatURL,
-          largeImageURL,
-        })
-      );
-      const currentData = [...this.state.galleryItems, ...newData];
-
-      this.setState(prevState => ({
-        galleryItems: [...prevState.galleryItems, ...newData],
+      const { totalHits, hits } = data;
+      const newData = hits.map(({ id, tags, webformatURL, largeImageURL }) => ({
+        id,
+        tags,
+        webformatURL,
+        largeImageURL,
       }));
+      const updatedGalleryItems = [...this.state.galleryItems, ...newData];
+      this.setState({
+        galleryItems: updatedGalleryItems,
+      });
 
-      if (!data.totalHits) {
+      if (!totalHits) {
         this.setState({ loading: false, error: true });
-        return toast.warn(
+        toast.warn(
           'Sorry, there are no images matching your search query. Please try again.'
         );
-      }
-
-      if (currentData.length >= data.totalHits) {
+      } else if (updatedGalleryItems.length >= totalHits) {
         this.setState({
           loading: false,
           isButtonShow: false,
           error: false,
         });
-        return;
+      } else if (nextPage === 1) {
+        toast.success(`Hooray! We found ${totalHits} images.`);
+        this.setState({
+          loading: false,
+          isButtonShow: true,
+          error: false,
+        });
+      } else {
+        this.setState({
+          loading: false,
+          isButtonShow: true,
+          error: false,
+        });
       }
-
-      if (nextPage === 1) {
-        toast.success(`Hooray! We found ${postApiService.hits} images.`);
-      }
-
-      this.setState({
-        loading: false,
-        isButtonShow: true,
-        error: false,
-      });
     });
   };
 
   handleFormSubmit = searchQuery => {
-    // викликається при відправці форми пошуку і оновлює `searchQuery` у стані компонента.
     this.setState({ searchQuery });
   };
 
   onLoadMore = () => {
-    // викликається при натисканні на кнопку "Load More" і збільшує значення `galleryPage` у стані компонента.
     this.setState(prevState => ({
       galleryPage: prevState.galleryPage + 1,
     }));
   };
 
   render() {
-    // рендерить компонент і його дочірні елементи, включаючи пошукову панель,
-    // галерею зображень, індикатор завантаження та кнопку "Load More".
     const { galleryItems, loading, isButtonShow, error } = this.state;
-
     return (
       <AppContent>
         <Searchbar onSubmit={this.handleFormSubmit} />
-        {/* відображає компонент `Searchbar` і передає йому функцію `handleFormSubmit` як властивість `onSubmit`. */}
         {!error && <ImageGallery galleryItems={galleryItems} />}
-        {/* відображає компонент `ImageGallery` лише в тому випадку, якщо `error` дорівнює `false`.  */}
         {loading && <Loader />}
-        {/*  відображає компонент `Loader` лише тоді, коли `loading` дорівнює `true` */}
         {isButtonShow && <Button onClick={this.onLoadMore} />}
-        {/*  відображає компонент `Button` лише тоді, коли `isButtonShow` дорівнює `true`. */}
         <ToastContainer autoClose={3000} theme="dark" />
       </AppContent>
-      // компонент ToastContainer використовується для відображення спливаючих повідомлень з
-      // допомогою бібліотеки `react-toastify`
-      // використовується темна тема (theme="dark"), і кожен
-      // тост автоматично закривається після 3 секунд (autoClose={3000}).
     );
   }
 }
